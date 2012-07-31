fcb05190a257d86dbe2de4305c2522f14d335b3b
Philipp Seidel <phibo@dinotools.org>
2012-08-01 00:43:36 +0200

    test - Adding unit tests

---
+++ tests/test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import unittest
+
+import ssdeep
+
+class FunctionTest(unittest.TestCase):
+    def testComputeHash(self):
+        self.assertEqual(
+            ssdeep.hash("Also called fuzzy hashes, Ctph can match inputs that have homologies."),
+            "3:AXGBicFlgVNhBGcL6wCrFQEv:AXGHsNhxLsr2C"
+        )
+
+        self.assertEqual(
+            ssdeep.hash("Also called fuzzy hashes, CTPH can match inputs that have homologies."),
+            "3:AXGBicFlIHBGcL6wCrFQEv:AXGH6xLsr2C"
+        )
+
+    def testCompareHash(self):
+        self.assertEqual(
+            ssdeep.compare(
+                "3:AXGBicFlgVNhBGcL6wCrFQEv:AXGHsNhxLsr2C",
+                "3:AXGBicFlIHBGcL6wCrFQEv:AXGH6xLsr2C"
+            ),
+            22
+        )
+
+    def testComputeHashFromFile(self):
+        self.assertEqual(
+            ssdeep.hash_from_file("test-file.txt"),
+            "3:AXGBicFlgVNhBGcL6wCrFQE3:AXGHsNhxLsr2s"
+        )
+
+if __name__ == "__main__":
+    unittest.main()
