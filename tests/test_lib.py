f08590db4be2d509e609cc0c512cb50bbd5de287
PhiBo <phibo@dinotools.org>
2014-06-08 11:40:33 +0200

    test - Add test for compare()

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -11,6 +11,31 @@ class TestFunctions(object):
         )
         assert res == 22
 
+        res = ssdeep.compare(
+            b"3:AXGBicFlgVNhBGcL6wCrFQEv:AXGHsNhxLsr2C",
+            b"3:AXGBicFlIHBGcL6wCrFQEv:AXGH6xLsr2C"
+        )
+        assert res == 22
+
+    def test_compare_fail(self):
+        with pytest.raises(TypeError):
+            ssdeep.compare(
+                "3:AXGBicFlgVNhBGcL6wCrFQEv:AXGHsNhxLsr2C",
+                None
+            )
+
+        with pytest.raises(TypeError):
+            ssdeep.compare(
+                None,
+                "3:AXGBicFlIHBGcL6wCrFQEv:AXGH6xLsr2C"
+            )
+
+        with pytest.raises(ssdeep.BaseError):
+            ssdeep.compare(
+                "3:AXGBicFlgVNhBGcL6wCrFQEv:AXGHsNhxLsr2C",
+                ""
+            )
+
     def test_hash_1(self):
         res = ssdeep.hash("Also called fuzzy hashes, Ctph can match inputs that have homologies.")
         assert res == "3:AXGBicFlgVNhBGcL6wCrFQEv:AXGHsNhxLsr2C"
