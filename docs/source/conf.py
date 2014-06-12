0dcf6ab67c532adf675c4ed6a038fafc1ded16e0
PhiBo <phibo@dinotools.org>
2014-06-12 23:57:42 +0200

    doc - Fix mock to build on RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ class Mock(object):
     def __getattr__(cls, name):
         if name in ('__file__', '__path__'):
             return '/dev/null'
-        elif name[0] == name[0].upper():
+        elif name[0] == name[0].upper() and name[0] not in ('FFI'):
             mockType = type(name, (), {})
             mockType.__module__ = __name__
             return mockType
