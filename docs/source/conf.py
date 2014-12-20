15d1d5b9e30ae2f0f66e2c6db1238dab0f44ffd3
PhiBo <phibo@dinotools.org>
2014-12-20 21:06:17 +0100

    doc - Build docs without six

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['cffi']
+MOCK_MODULES = ['cffi', 'six']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
