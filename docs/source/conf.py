78eb5bdbb668866aac6af6479f5efd4b80d3924c
PhiBo <phibo@dinotools.org>
2015-06-10 21:24:08 +0200

    doc - Use mock module to build docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,30 +16,13 @@
 import sys
 import os
 
-class Mock(object):
-
-    __all__ = []
-
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        return Mock()
-
-    @classmethod
-    def __getattr__(cls, name):
-        if name in ('__file__', '__path__'):
-            return '/dev/null'
-        elif name[0] == name[0].upper() and name[0] not in ('FFI'):
-            mockType = type(name, (), {})
-            mockType.__module__ = __name__
-            return mockType
-        else:
-            return Mock()
-
 MOCK_MODULES = ['cffi', 'six']
 for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = Mock()
+    try:
+        __import__(mod_name)
+    except:
+        import mock
+        sys.modules[mod_name] = mock.MagicMock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
