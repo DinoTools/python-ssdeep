7e667c9fd5f1c6a33da247bb46f9e66bcefe3ca6
PhiBo <phibo@dinotools.org>
2020-10-29 06:07:25 +0100

    src - Remove deprecated ffi.verify()
    - Restructuring the source to remove ffi.verify()
    - Some options are still hard coded

--- ssdeep/_build.py
+++ ssdeep/_build.py
@@ -1,8 +1,5 @@
-import binascii
+import cffi
 
-from cffi import FFI
-
-from ssdeep.__about__ import __version__
 
 cdef = """
     static const long FUZZY_FLAG_ELIMSEQ;
@@ -106,50 +103,14 @@ CONDITIONAL_NAMES = {
     )
 }
 
-
-class Binding(object):
-    def __init__(self, extra_objects=None, include_dirs=None, libraries=None):
-        self.ffi = FFI()
-        self.ffi.cdef(cdef)
-        self._lib = None
-        if extra_objects is None:
-            extra_objects = []
-        self._extra_objects = extra_objects
-        if include_dirs is None:
-            include_dirs = []
-        self._include_dirs = include_dirs
-        if libraries is None:
-            libraries = ["fuzzy"]
-        self._libraries = libraries
-
-    def verify(self):
-        self._lib = self.ffi.verify(
-            source,
-            ext_package="ssdeep",
-            extra_objects=self._extra_objects,
-            include_dirs=self._include_dirs,
-            modulename=_create_modulename(cdef, source, __version__),
-            libraries=self._libraries,
-        )
-
-        for condition, names in CONDITIONAL_NAMES.items():
-            if getattr(self._lib, condition):
-                continue
-            for name in names:
-                delattr(self._lib, name)
-
-    @property
-    def lib(self):
-        if self._lib is None:
-            self.verify()
-        return self._lib
+ffi = cffi.FFI()
+ffi.set_source(
+    "ssdeep._libfuzzy",
+    source,
+    extra_link_args=["-lfuzzy"],
+)
+ffi.cdef(cdef)
 
 
-def _create_modulename(cdef, source, sys_version):
-    key = "\x00".join([sys_version[:3], source, cdef])
-    key = key.encode("utf-8")
-    k1 = hex(binascii.crc32(key[0::2]) & 0xffffffff)
-    k1 = k1.lstrip("0x").rstrip("L")
-    k2 = hex(binascii.crc32(key[1::2]) & 0xffffffff)
-    k2 = k2.lstrip("0").rstrip("L")
-    return "_ssdeep_cffi_{0}{1}".format(k1, k2)
+if __name__ == '__main__':
+    ffi.compile()
