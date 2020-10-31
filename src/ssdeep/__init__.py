5a7f7842120f61e4b468da3032556a241fa1613a
PhiBo <phibo@dinotools.org>
2020-10-31 06:25:21 +0100

    src - Skip flake8 for about imports

--- src/ssdeep/__init__.py
+++ src/ssdeep/__init__.py
@@ -7,7 +7,8 @@ import os
 
 import six
 
-from ssdeep.__about__ import (
+# Ignore flake8 F401 warning for unused vars
+from ssdeep.__about__ import (  # noqa: F401
     __author__, __copyright__, __email__, __license__, __summary__, __title__,
     __uri__, __version__
 )
